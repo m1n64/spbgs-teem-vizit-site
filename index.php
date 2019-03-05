06117070a63ec4817f8de68ab31f28ac2809b8af
m1n64 <mrcaxapov@gmail.com>
2019-03-06 01:15:52 +0300

    v2

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+    <script type="libs/jquery-3.2.1.min.js"></script>
+    
+  </body>
+</html>
