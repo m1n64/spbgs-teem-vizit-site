df82c5e39aaa9876b7f523db022bc8e0d77915be
m1n64 <mrcaxapov@gmail.com>
2019-03-06 01:56:18 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@
 
   anime({
     targets: "#navbar",
-    backgroundColor: ["#e91e63", "#f44336", "#9575cd", "#00897b", "#a5d6a7", "#dce775"],
-    duration: 3000,
+    backgroundColor: ["#e91e63", "#f44336", "#9575cd", "#00897b", "#a5d6a7", "#dce775", "#fff59d", "#ffcc80", "#ffab91"],
+    duration: 50000,
     loop: true,
     direction: 'alternate'
   });
