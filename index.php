188f9470dcf3e531d840cb5f1df9a4ee988c5faf
m1n64 <mrcaxapov@gmail.com>
2019-03-06 22:12:31 +0300

    v0.4

--- index.php
+++ index.php
@@ -3,143 +3,30 @@
   <head>
     <base href="<?php echo $_SERVER['REQUEST_SCHEME'] . '://' .$_SERVER['SERVER_NAME'].'/';  ?>" />
     <meta charset="utf-8">
-    <title></title>
+    <meta name="theme-color" content="#202020">
+    <title>BRICK TEAM | Portfolio</title>
 
-    <link rel="stylesheet" href="libs/material/css/materialize.min.css">
+    <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+    <!-- <link rel="stylesheet" href="libs/material/css/materialize.min.css"> -->
+    <link rel="stylesheet" href="libs/bootsrtap-grid/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/fonts/style.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/index.css">
   </head>
   <body>
-    <div class="navbar-fixed">
-    <nav>
-  <div class="nav-wrapper" id="navbar">
-    <a href="#" class="brand-logo">Logo</a>
-    <a href="#" data-target="slide-out" class="sidenav-trigger hide-on-large-only"><i class="material-icons">menu</i></a>
-    <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <li><a href="collapsible.html">JavaScript</a></li>
-    </ul>
-  </div>
-</nav>
+    <div class="navbar">
+      <div class="glitch" data-text="BRICK">BRICK</div>
+      <!-- <div class="glitch mini" data-text="TEAM">TEAM</div> -->
+    </div>
+    <div class="divider-scroll"><span class="icon-scroll" for="#content-scroll" id="main-scroll"></span></div>
 
-</div>
-
-
-<ul id="slide-out" class="sidenav">
-  <li><a href="sass.html">Sass</a></li>
-  <li><a href="badges.html">Components</a></li>
-  <li><a href="collapsible.html">JavaScript</a></li>
-  </ul>
-
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-<p>1</p>
-
-        <footer class="page-footer" id="footer">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-            </div>
-          </div>
-        </footer>
+    <div class="row" id="content-scroll">
+      
+    </div>
 
     <script src="libs/jquery-3.2.1.min.js"></script>
-    <script src="libs/material/js/materialize.min.js"></script>
+    <script src="libs/bootsrtap-grid/js/bootstrap.min.css"></script>
+    <!-- <script src="libs/material/js/materialize.min.js"></script> -->
     <script src="libs/anime.min.js"></script>
     <script src="js/index.js"></script>
   </body>
--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 (function(){
-  $('.sidenav').sidenav();
-
+  // $('.sidenav').sidenav();
+  //
   anime({
-    targets: ["#navbar", "#footer"],
-    backgroundColor: ["#e91e63", "#f44336", "#9575cd", "#00897b", "#a5d6a7", "#dce775", "#fff59d", "#ffcc80", "#ffab91"],
+    targets: ".divider-scroll",
+    backgroundColor: ["#48004e", "#510058", "#3d0042", "#580060"],//["#e91e63", "#f44336", "#9575cd", "#00897b", "#a5d6a7", "#dce775", "#fff59d", "#ffcc80", "#ffab91"],
     duration: 50000,
     loop: true,
     direction: 'alternate'
+  });
+
+  $("#main-scroll").click(function(){
+    let sc = $(this).attr("for");
+    $('html, body').animate({scrollTop: $(`${sc}`).offset().top}, 500);
   });
 })(jQuery);
