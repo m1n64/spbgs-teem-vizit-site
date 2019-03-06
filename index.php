d17a95728651724ba09f733aab4f5ca9cf7bcb0b
m1n64 <mrcaxapov@gmail.com>
2019-03-07 02:20:10 +0300

    v 0.98

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
     <!-- <link rel="stylesheet" href="libs/material/css/materialize.min.css"> -->
     <link rel="stylesheet" href="libs/bootsrtap-grid/css/bootstrap-grid.min.css">
+    <!-- <link rel="stylesheet" href="libs/scrollbar/perfect-scrollbar.css"> -->
+    <link rel="stylesheet" href="css/animations.css">
     <link rel="stylesheet" href="css/fonts/style.css">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/index.css">
@@ -20,12 +22,101 @@
     </div>
     <div class="divider-scroll"><span class="icon-scroll" for="#content-scroll" id="main-scroll"></span></div>
 
-    <div class="row" id="content-scroll">
-      
+    <div id="content-scroll">
+      <div class="row">
+        <div class="col-sm-12 col-lg-4">
+          <div class="card-content" id="card1">
+            <div class="card-head"><span class="card-icon icon-embed"></span><span class="card-name">Кирилл Сахаров</span></div>
+            <span class="card-text">Гланвый разработчик в комманде. Иногда верстальщик, иногда дизайнер. В основном занимается Back-End разработкой.</span>
+          </div>
+        </div>
+        <div class="col-sm-12 col-lg-4">
+          7
+        </div>
+        <div class="col-sm-12 col-lg-4">
+      t
+        </div>
+        <div class="col-sm-12 col-lg-4">
+t
+        </div>
+        <div class="col-sm-12 col-lg-4">
+t
+        </div>
+        <div class="col-sm-12 col-lg-4">
+t
+        </div>
     </div>
+</div>
 
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+
+<div id="scrolldown"></div>
     <script src="libs/jquery-3.2.1.min.js"></script>
-    <script src="libs/bootsrtap-grid/js/bootstrap.min.css"></script>
+    <script src="libs/bootsrtap-grid/js/bootstrap.min.js"></script>
+    <!-- <script src="libs/scrollbar/perfect-scrollbar.min.js"></script> -->
     <!-- <script src="libs/material/js/materialize.min.js"></script> -->
     <script src="libs/anime.min.js"></script>
     <script src="js/index.js"></script>
--- js/index.js
+++ js/index.js
@@ -1,16 +1,32 @@
-(function(){
+(function() {
   // $('.sidenav').sidenav();
   //
+
+  $('html, body').animate({
+    scrollTop: 0
+  }, 100);
+
   anime({
     targets: ".divider-scroll",
-    backgroundColor: ["#48004e", "#510058", "#3d0042", "#580060"],//["#e91e63", "#f44336", "#9575cd", "#00897b", "#a5d6a7", "#dce775", "#fff59d", "#ffcc80", "#ffab91"],
+    backgroundColor: ["#48004e", "#510058", "#3d0042", "#580060"], //["#e91e63", "#f44336", "#9575cd", "#00897b", "#a5d6a7", "#dce775", "#fff59d", "#ffcc80", "#ffab91"],
     duration: 50000,
     loop: true,
     direction: 'alternate'
   });
 
-  $("#main-scroll").click(function(){
+  $("#main-scroll").click(function() {
     let sc = $(this).attr("for");
-    $('html, body').animate({scrollTop: $(`${sc}`).offset().top}, 500);
+    $("#content-scroll").animate({opacity: 1}, 500);
+    $('html, body').animate({
+      scrollTop: $(`${sc}`).offset().top
+    }, 500);
+    $('body').css("overflow", "scroll").css("overflow-x", "hidden");
   });
+
+  //
+  // window.addEventListener('wheel', function (e) {
+  //   var parent = document.getElementById('content-scroll');
+  // var child = document.getElementById('scrolldown');
+  // child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
+  // });
 })(jQuery);
