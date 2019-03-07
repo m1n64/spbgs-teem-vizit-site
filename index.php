a8ddfe334379d92374d76a81c829da19d1763998
m1n64 <mrcaxapov@gmail.com>
2019-03-07 15:20:53 +0300

    version 1.1

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+  $lng = trim($_GET['lng']);
+
+  $lang = [];
+
+  if (isset($_COOKIE['lang'])){
+    $lang = getLang($_COOKIE['lang']);
+  }
+  else {
+    $lang = getLang($lng);
+  }
+
+  function getLang($lng){
+    switch($lng) {
+      default:
+      case "eng":
+        return json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/langs/en_EN.json"));
+      case "ru":
+        return json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/langs/ru_RU.json"));
+    }
+  }
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -21,32 +43,75 @@
       <!-- <div class="glitch mini" data-text="TEAM">TEAM</div> -->
     </div>
     <div class="divider-scroll"><span class="icon-scroll" for="#content-scroll" id="main-scroll"></span></div>
+    <div class="lang" id="changelang">
+      <a href="" lang="eng" class="lang_name">English</a>
+      <a href="" lang="ru" class="lang_name">Русский</a>
+    </div>
 
     <div id="content-scroll">
       <div class="row">
-        <div class="col-sm-12 col-lg-4">
+        <div class="col-sm-12 col-md-6 col-lg-4">
           <div class="card-content" id="card1">
-            <div class="card-head"><span class="card-icon icon-embed"></span><span class="card-name">Кирилл Сахаров</span></div>
-            <span class="card-text">Гланвый разработчик в комманде. Иногда верстальщик, иногда дизайнер. В основном занимается Back-End разработкой.</span>
+            <div class="card-head"><span class="card-icon icon-embed"></span><span class="card-name"><?php echo $lang->card1->name; ?></span></div>
+            <div><span class="card-text"><?php echo $lang->card1->desc; ?></span></div>
+            <div class="card-links">
+              <a href="https://vk.com/spbg_grib" class="vk"><span class="icon-vk"></span> VK</a>
+            </div>
           </div>
         </div>
-        <div class="col-sm-12 col-lg-4">
-          7
+        <div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card-content" id="card2">
+            <div class="card-head"><span class="card-icon icon-laptop"></span><span class="card-name"><?php echo $lang->card2->name; ?></span></div>
+            <div><span class="card-text"><?php echo $lang->card2->desc; ?></span></div>
+            <div class="card-links">
+              <a href="https://vk.com/spikuza" class="vk"><span class="icon-vk"></span> VK</a>
+            </div>
+          </div>
         </div>
-        <div class="col-sm-12 col-lg-4">
-      t
+        <div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card-content" id="card3">
+            <div class="card-head"><span class="card-icon icon-power"></span><span class="card-name"><?php echo $lang->card3->name; ?></span></div>
+            <div><span class="card-text"><?php echo $lang->card3->desc; ?></span></div>
+            <div class="card-links">
+              <a href="https://vk.com/pyrkh1" class="vk"><span class="icon-vk"></span> VK</a>
+            </div>
+          </div>
         </div>
-        <div class="col-sm-12 col-lg-4">
-t
+        <div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card-content" id="card4">
+            <div class="card-head"><span class="card-icon icon-pencil2"></span><span class="card-name"><?php echo $lang->card4->name; ?></span></div>
+            <div><span class="card-text"><?php echo $lang->card4->desc; ?></span></div>
+            <div class="card-links">
+              <a href="https://vk.com/powerofstrike" class="vk"><span class="icon-vk"></span> VK</a>
+            </div>
+          </div>
         </div>
-        <div class="col-sm-12 col-lg-4">
-t
-        </div>
-        <div class="col-sm-12 col-lg-4">
-t
+        <div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card-content" id="card5">
+            <div class="card-head"><span class="card-icon icon-fire red"></span><span class="card-name"><?php echo $lang->card5->name; ?></span></div>
+            <div><span class="card-text"><?php echo $lang->card5->desc; ?></span></div>
+            <div class="card-links">
+              <a href="https://vk.com/id94947434" class="vk"><span class="icon-vk"></span> VK</a>
+            </div>
         </div>
     </div>
 </div>
+
+<!-- экономика
+технико-экономическое обоснование. Внедрение программы "Информационно-справочная система по программе Adobe Dreamweaver"
+
+Введение:
+Значение автоматизации в жизни общества
+Цель: получение практических навыков по расчёту экономических показателей разработки программы, а так же закрепление теоретических знаний, полученных в ходе изучение дисцпилины "Экномоика организаций"
+
+1. Экономически обосновать постановку задач
+2. Определить трудоёмкость разработчки программы
+3. Определить затраты на разработку ПО
+4. Определить свободно отпускную цену программы
+
+3 главы.
+список источников.
+-->
 
 <p>1</p>
 <p>1</p>
@@ -119,6 +184,7 @@ t
     <!-- <script src="libs/scrollbar/perfect-scrollbar.min.js"></script> -->
     <!-- <script src="libs/material/js/materialize.min.js"></script> -->
     <script src="libs/anime.min.js"></script>
+    <script src="js/functions.js"></script>
     <script src="js/index.js"></script>
   </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -16,13 +16,19 @@
 
   $("#main-scroll").click(function() {
     let sc = $(this).attr("for");
-    $("#content-scroll").animate({opacity: 1}, 500);
+    $("#content-scroll").animate({
+      opacity: 1
+    }, 500);
     $('html, body').animate({
       scrollTop: $(`${sc}`).offset().top
     }, 500);
     $('body').css("overflow", "scroll").css("overflow-x", "hidden");
   });
 
+  $(".lang_name").click(function(){
+    let lang = $(this).attr('lang');
+    setCookie("lang", "/", lang, 365);
+  });
   //
   // window.addEventListener('wheel', function (e) {
   //   var parent = document.getElementById('content-scroll');
